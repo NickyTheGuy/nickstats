e745337978a7d24377b65beaa14ca05a943cab7c
Nicholas Kenney <47020046+NickyTheGuy@users.noreply.github.com>
2026-09-04 16:31:08 -0400

    Refresh worker for trade diagnostics

--- js/demo.js
+++ js/demo.js
@@ -65,7 +65,7 @@
     state.workerReady = new Promise((resolve, reject) => {
       state.resolveReady = resolve;
       state.rejectReady = reject;
-      const worker = new Worker("./js/demo-worker.js?v=20260904-7");
+      const worker = new Worker("./js/demo-worker.js?v=20260904-8");
       state.worker = worker;
       const timeout = setTimeout(() => {
         const error = new Error("The demo parser took too long to start.");
