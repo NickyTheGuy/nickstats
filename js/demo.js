738d68d4cc5dc0096e63357e4ed6b838a256c14d
Nicholas Kenney <47020046+NickyTheGuy@users.noreply.github.com>
2026-09-04 16:16:06 -0400

    Refresh parser worker after KAST fix

--- js/demo.js
+++ js/demo.js
@@ -65,7 +65,7 @@
     state.workerReady = new Promise((resolve, reject) => {
       state.resolveReady = resolve;
       state.rejectReady = reject;
-      const worker = new Worker("./js/demo-worker.js?v=20260904-5");
+      const worker = new Worker("./js/demo-worker.js?v=20260904-6");
       state.worker = worker;
       const timeout = setTimeout(() => {
         const error = new Error("The demo parser took too long to start.");
