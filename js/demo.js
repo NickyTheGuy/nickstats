929f4f261b7341b8c7a169411a0aa4409128737b
Nicholas Kenney <47020046+NickyTheGuy@users.noreply.github.com>
2026-09-04 16:24:00 -0400

    Refresh worker for first-half KAST fix

--- js/demo.js
+++ js/demo.js
@@ -65,7 +65,7 @@
     state.workerReady = new Promise((resolve, reject) => {
       state.resolveReady = resolve;
       state.rejectReady = reject;
-      const worker = new Worker("./js/demo-worker.js?v=20260904-6");
+      const worker = new Worker("./js/demo-worker.js?v=20260904-7");
       state.worker = worker;
       const timeout = setTimeout(() => {
         const error = new Error("The demo parser took too long to start.");
