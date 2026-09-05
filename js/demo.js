47b2963154d15eabd55bea78abbf5e38308e4c5b
Nicholas Kenney <47020046+NickyTheGuy@users.noreply.github.com>
2026-09-05 12:47:05 -0400

    Refresh side-aware demo worker

--- js/demo.js
+++ js/demo.js
@@ -163,7 +163,7 @@
     state.workerReady = new Promise((resolve, reject) => {
       state.resolveReady = resolve;
       state.rejectReady = reject;
-      const worker = new Worker("./js/demo-worker.js?v=20260905-21");
+      const worker = new Worker("./js/demo-worker.js?v=20260905-22");
       state.worker = worker;
       const timeout = setTimeout(() => {
         const error = new Error("The demo parser took too long to start.");
