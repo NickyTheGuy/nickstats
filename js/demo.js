8cf857aa8ccf4783514cea58410acd708ebea399
Nicholas Kenney <47020046+NickyTheGuy@users.noreply.github.com>
2026-09-05 14:31:35 -0400

    Distinguish USP-S from P2000 in weapon usage

--- js/demo.js
+++ js/demo.js
@@ -169,7 +169,7 @@
     state.workerReady = new Promise((resolve, reject) => {
       state.resolveReady = resolve;
       state.rejectReady = reject;
-      const worker = new Worker("./js/demo-worker.js?v=20260905-29");
+      const worker = new Worker("./js/demo-worker.js?v=20260905-30");
       state.worker = worker;
       const timeout = setTimeout(() => {
         const error = new Error("The demo parser took too long to start.");
