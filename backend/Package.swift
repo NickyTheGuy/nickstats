2336cc6a38c9a890c2b21d7ea2a729c9b385b01d
Nicholas Kenney <47020046+NickyTheGuy@users.noreply.github.com>
2026-09-10 19:26:09 -0400

    Add NIO SSL for MySQL TLS

--- backend/Package.swift
+++ backend/Package.swift
@@ -12,7 +12,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.13.0"),
         .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.8.0"),
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.36.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "4.5.2")
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "4.5.2"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.30.0")
     ],
     targets: [
         .executableTarget(
@@ -22,7 +23,8 @@ let package = Package(
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
                 .product(name: "SQLKit", package: "sql-kit"),
-                .product(name: "Crypto", package: "swift-crypto")
+                .product(name: "Crypto", package: "swift-crypto"),
+                .product(name: "NIOSSL", package: "swift-nio-ssl")
             ]
         ),
         .testTarget(
@@ -34,4 +36,3 @@ let package = Package(
         )
     ]
 )
-
